a70c0cb2cd195b77e4e646d89e444c8203205e81
Vinicius Fraga <viniciusfrg@outlook.com.br>
2022-12-05 23:31:04 -0300

    Project Calculator

---
+++ Calculadora/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora {
+    public partial class MainPage : ContentPage {
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+        public MainPage()
+        {
+            InitializeComponent();
+            OnClear(new object(), new EventArgs());
+        }
+
+        void OnClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.resultText.Text = "0";
+        }
+
+        void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            string pressed = button.Text;
+
+            if (this.resultText.Text == "0" || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if (currentState < 0)
+                {
+                    currentState *= -1;
+                }
+            }
+            this.resultText.Text += pressed;
+
+            double number;
+            if (double.TryParse(this.resultText.Text, out number))
+            {
+                this.resultText.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+
+        }
+
+        void OnCalculate(object sender, EventArgs e)
+        {
+            if (currentState == 2)
+            {
+                double result = 0;
+                if (mathOperator == "+")
+                {
+                    result = firstNumber + secondNumber;
+                }
+                if (mathOperator == "-")
+                {
+                    result = firstNumber - secondNumber;
+                }
+                if (mathOperator == "X")
+                {
+                    result = firstNumber * secondNumber;
+                }
+                if (mathOperator == "/")
+                {
+                    result = firstNumber / secondNumber;
+                }
+                this.resultText.Text = result.ToString("N0");
+                firstNumber = result;
+                currentState = -1;
+            }
+        }
+
+        void OnSelectOperator(object sender, EventArgs e)
+        {
+            currentState = -2;
+            Button button = sender as Button;
+            string pressed = button.Text;
+            mathOperator = pressed;
+        }
+
+
+
+    }
+}
